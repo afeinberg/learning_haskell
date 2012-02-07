502d2685597726354d13e38090ee4e1174eaf07f
Alex Feinberg <alex@strlen.net>
2012-02-07 01:00:47 -0800

    Add cs240h lab1

---
+++ Lab1/Histogram.hs
@@ -0,0 +1,39 @@
+module Histogram
+       where
+
+import qualified Data.List as List
+import qualified Data.Map as Map
+
+type Histogram a = [(a, Int)]
+
+compute :: (Ord a) => [a] -> Histogram a
+compute lst =
+  let incOrAdd e m = Map.insertWith (\_ o -> o + 1) e 1 m
+      counts = foldr incOrAdd Map.empty lst
+      compareCounts (_, a) (_, b) = compare b a
+  in List.sortBy compareCounts (Map.toList counts)
+   
+display :: (Show a) => Histogram a -> Int -> String
+display hst maxCols =   
+  let maxCount = snd (List.head hst)
+      showNames = map (show . fst) hst
+      maxLen = maximum (map length showNames)
+      byRow acc (i, c) = 
+        case (showBars (maxCols - maxLen - 1) maxCount c) of
+          Nothing -> acc
+          Just bars -> acc ++ (pad (show i) maxLen) ++ bars ++ "\n"          
+  in foldl byRow "" hst
+  
+pad :: String -> Int -> String     
+pad str len = 
+  let spaces = len - (length str) + 1
+  in str ++ (replicate spaces ' ')
+    
+showBars :: Int -> Int -> Int -> Maybe String
+showBars cols maxCount count = 
+  let wordsPerBar = (fromIntegral cols) / (fromIntegral maxCount)
+      numBars = round $ wordsPerBar * (fromIntegral count)
+  in if (numBars > 0) then Just (replicate numBars '*')
+     else Nothing
+          
+  
---
+++ Lab1/Lab1.hs
@@ -0,0 +1,28 @@
+module Main
+       where
+
+import IO
+import System.Environment
+
+import qualified Char
+import qualified Histogram
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as TIO
+
+contentsToList :: T.Text -> [T.Text]
+contentsToList contents = 
+  let discardFirst str | T.null str = str
+                       | Char.isAlpha (T.head str) = str
+                       | otherwise = T.tail str
+      discardLast str | Char.isAlpha (T.last str) = str
+                      | otherwise = T.init str          
+      lst = map (T.toLower . discardFirst . discardLast) (T.words contents)
+  in filter (not . T.null) lst
+
+main :: IO ()
+main = do
+  (filename:_) <- getArgs
+  contents <- TIO.readFile filename
+  let hst = Histogram.compute (contentsToList contents)
+  putStrLn (Histogram.display hst 80)
+  
